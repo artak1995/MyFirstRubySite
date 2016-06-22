b6b4921491fc27f485ce999f25e0094cf81bea0b
Billy <artak1995@gmail.com>
2016-06-22 17:19:36 +0800

    create the layout of the web app

--- mysite/config/initializers/assets.rb
+++ mysite/config/initializers/assets.rb
@@ -8,4 +8,15 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+#
+Rails.application.config.assets.precompile += %w( test.css )
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( site_theme.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( cbpAnimatedHeader.js )
+Rails.application.config.assets.precompile += %w( jquery.js )
+Rails.application.config.assets.precompile += %w( classie.js )
+
+
+
+
--- mysite/config/routes.rb
+++ mysite/config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  resources :todos
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # root 'welcome#say'
+  resources :todos
+  root 'welcome#test'
+
+  get "welcome/say_hello" => "welcome#say"
+  get "welcome" => "welcome#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
